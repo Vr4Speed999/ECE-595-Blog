b9e896ebfa08873c712b55894673c18fc0232378
Dustin Oldham <Bart@unm.edu>
2011-10-02 14:39:01 -0600

    ECE Blog Files

---
+++ myBlog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyBlog::Application.config.secret_token = 'e73b7342f404597b29e650a9cafd462e79235441eaae347f3fc35f9a9be8d20ea5e8d51530dd2d7dd56c2ced2cfc250f397ee0195527eff3e63bad4a44a80d21'
